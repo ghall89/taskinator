6f4a2cbb0150e8af4a9ecf5ca31bc051252e313f
Graham Hall <ghall89@me.com>
2021-02-15 20:39:19 -0500

    refactored functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var taskFormHandler = function () {
     // prevents default browser behavior for button click
     event.preventDefault();
 
@@ -9,16 +9,25 @@ var createTaskHandler = function () {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    // create a list item and give it the class of 'task-item'
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    // create div within list item and give it the class of 'task-info'
-    var taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    // writes html into the div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-    listItemEl.appendChild(taskInfoEl);
-    tasksToDoEl.appendChild(listItemEl);
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+var createTaskEl = function(taskDataObj) {
+   // create a list item and give it the class of 'task-item'
+   var listItemEl = document.createElement("li");
+   listItemEl.className = "task-item";
+   // create div within list item and give it the class of 'task-info'
+   var taskInfoEl = document.createElement("div");
+   taskInfoEl.className = "task-info";
+   // writes html into the div
+   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+   listItemEl.appendChild(taskInfoEl);
+   tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", taskFormHandler);
