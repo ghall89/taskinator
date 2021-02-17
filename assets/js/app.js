a79cf79f0b0b4c78f310973ba4bf1b7e42d34319
Graham Hall <ghall89@me.com>
2021-02-17 10:56:49 -0500

    added delete button functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskIdCounter = 0;
 
+var pageContentEl = document.querySelector("#page-content");
+
 var taskFormHandler = function () {
     // prevents default browser behavior for button click
     event.preventDefault();
@@ -93,4 +95,20 @@ var createTaskActions = function(taskId) {
     return actionContainerEl;
 }
 
+var taskButtonHandler = function(event) {
+    
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+}
+
+
+// Event listeners
 formEl.addEventListener("submit", taskFormHandler);
+pageContentEl.addEventListener("click", taskButtonHandler);
